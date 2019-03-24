51b36bee0af47b0869c63ed220d58a2a4ba74582
Lucas Wonderley <lucas.r.wonderley@gmail.com>
2019-03-24 15:49:20 -0400

    Initial commit

---
+++ src/main.go
@@ -0,0 +1,73 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// [START speech_quickstart]
+
+// Sample speech-quickstart uses the Google Cloud Speech API to transcribe
+// audio.
+package main
+
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"log"
+
+	speech "cloud.google.com/go/speech/apiv1"
+	speechpb "google.golang.org/genproto/googleapis/cloud/speech/v1"
+)
+
+func main() {
+	ctx := context.Background()
+
+	// Creates a client.
+	client, err := speech.NewClient(ctx)
+	if err != nil {
+		log.Fatalf("Failed to create client: %v", err)
+	}
+
+	// Sets the name of the audio file to transcribe.
+	//filename := "../data/audio.raw"
+	filename := "../data/2017-03-12-StarWars7x7Show982-Tem7n.mp3"
+
+	// Reads the audio file into memory.
+	data, err := ioutil.ReadFile(filename)
+	if err != nil {
+		log.Fatalf("Failed to read file: %v", err)
+	}
+
+	// Detects speech in the audio file.
+	resp, err := client.Recognize(ctx, &speechpb.RecognizeRequest{
+		Config: &speechpb.RecognitionConfig{
+			Encoding:        speechpb.RecognitionConfig_LINEAR16,
+			SampleRateHertz: 16000,
+			LanguageCode:    "en-US",
+		},
+		Audio: &speechpb.RecognitionAudio{
+			AudioSource: &speechpb.RecognitionAudio_Content{Content: data},
+		},
+	})
+	if err != nil {
+		log.Fatalf("failed to recognize: %v", err)
+	}
+
+	// Prints the results.
+	for _, result := range resp.Results {
+		for _, alt := range result.Alternatives {
+			fmt.Printf("\"%v\" (confidence=%3f)\n", alt.Transcript, alt.Confidence)
+		}
+	}
+}
+
+// [END speech_quickstart]
